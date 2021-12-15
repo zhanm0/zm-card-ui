9d09b5fbc696da49acdcf85d4b917e532bc7bec6
zhanming <zhanming@weihaizixun.com>
2021-12-15 18:54:53 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  base: '/zm-card-ui',
+  title: 'zhanming-card-ui',
+  base: '/zm-card-ui/',
   themeConfig: {
     sidebar: [
       '/',
       '/componentsDocs/card',
       // '/page-a',
       // ['/page-b', 'Explicit link text']
+    ],
+    nav: [
+      {text: 'Home', link: '/'},
+      {text: 'Github', link: 'https://github.com/zhanm0/zm-card-ui'},
+      {text: 'Npm', link: 'https://www.npmjs.com/package/zm-card-ui'}
     ]
   }
 }
